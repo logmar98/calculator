29e7beb195ca91cb0227009a84566b5e02ef6e2a
logmar98 <marwanarbib15@gmail.com>
2022-11-25 19:34:53 +0100

    i finsh calculator structur in html and design in  css

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const subtract = (a,b)  =>  a - b;
 const multiply = (a,b)  =>  a * b;
 const divide = (a,b)  =>  a / b;
 
-
-
 function operate(a,operator,b) {
     return operator(a,b)
 }
 console.log(operate(5,divide,5))
+
+const screen = document.querySelector('.content')
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    screen.textcontent = `${button.id}`;
+    console.log(button.id);
+  });
+});
